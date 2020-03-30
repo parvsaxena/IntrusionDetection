83dd70b5cd72a6e4713f71d595676fd6347ce4b5
Parv <saxena.parv@gmail.com>
2020-03-30 01:42:21 -0400

    Fixing parsing bugs

--- analyzer.py
+++ analyzer.py
@@ -1,67 +1,101 @@
 from scapy.all import *
 from scapy.layers.l2 import Ether
-from scapy.layers.inet import IP, TCP, UDP
+from scapy.layers.inet import IP, TCP, UDP, ICMP, icmptypes
+from scapy.data import ETHER_TYPES
 
 # Time ??, Source IP(d), Destination IP(d), Protocol ??, Length(d),
 # Source Port(d), Dest Port(d), TTL(d), IP version ??
 # https://www.howtoinstall.co/en/ubuntu/xenial/tshark
 
+def expand(x):
+    yield x.name
+    while x.payload:
+        x = x.payload
+        yield x.name
+
+def extract_ip(parsed_dict, ip_pkt):
+    parsed_dict['ip_src'] = ip_pkt.src
+    parsed_dict['ip_dst'] = ip_pkt.dst
+    parsed_dict['ip_ttl'] = ip_pkt.ttl
+    parsed_dict['ip_len'] = ip_pkt.len
+    parsed_dict['ip_ver'] = ip_pkt.version
+    # TODO: prototype number to name translation
+    parsed_dict['proto'] = ip_pkt.proto
+
+def extract_ether(parsed_dict, ether_pkt):
+    parsed_dict['mac_src'] = ether_pkt.src
+    parsed_dict['mac_dst'] = ether_pkt.dst
+    parsed_dict['ether_type'] = ether_pkt.type
+    # https://github.com/secdev/scapy/blob/master/scapy/libs/ethertypes.py
+    # print(ETHER_TYPES['IPV4'])
+
+def extract_tcp(parsed_dict, tcp_pkt):
+    parsed_dict['src_port'] = tcp_pkt.sport
+    parsed_dict['dst_port'] = tcp_pkt.dport
+
+def extract_udp(parsed_dict, udp_pkt):
+    parsed_dict['src_port'] = udp_pkt.sport
+    parsed_dict['dst_port'] = udp_pkt.dport
+    parsed_dict['udp_len'] = udp_pkt.len
+
+def extract_icmp(parsed_dict, icmp_pkt):
+    print(icmp_pkt.summary())
+    # TODO: Ask which more fields are necessary
+    parsed_dict['icmp_type'] = icmp_pkt.type  # 0 for request, 8 for reply
+    parsed_dict['icmp_type_str'] = icmptypes[icmp_pkt.type]
+    parsed_dict['icmp_code'] = icmp_pkt.code  # code field which gives extra information about icmp type
+    
+def extract_arp(parsed_dict, arp_pkt):
+    print(arp_pkt.summary())
+    parsed_dict['arp_op'] = arp_pkt.op # 1 who-has, 2 is-at
+    parsed_dict['arp_psrc'] = arp_pkt.psrc
+    parsed_dict['arp_pdst'] = arp_pkt.pdst
+    parsed_dict['arp_hwsrc'] = arp_pkt.hwsrc
+    parsed_dict['arp_hwdst'] = arp_pkt.hwdst
+
 def parse_packet(pkt_data):
-    ip_pkt = IP(pkt_data)
-    dict = {}
-    # print(type(ip_pkt))
-    if ip_pkt is not None:
+    print(list(expand(pkt_data)))   
+    #print(ip_pkt.show())
+    # TODO: Initialize data
+    parsed_dict = {}
+    # print(tape(ip_pkt))
+    if pkt_data.haslayer(Ether):
+        print("Ether layer exists")
+        ether_pkt = pkt_data.getlayer(Ether)
+        extract_ether(parsed_dict, ether_pkt)
+    # TODO: Extract ports
+    # TODO: Do we need IPv6 support?
+    # IP packet
+    if pkt_data.haslayer(IP):
+        print("Received IP packet")
+        ip_pkt = pkt_data.getlayer(IP)
+        extract_ip(parsed_dict, ip_pkt)
         # print(ip_pkt.show())
-        dict['ip_src'] = ip_pkt.src
-        dict['ip_dst'] = ip_pkt.dst
-        # TODO: fix ttl
-        dict['ip_ttl'] = ip_pkt.ttl
-        dict['ip_len'] = ip_pkt.len
-        # TODO: version seems off
-        dict['ip_ver'] = ip_pkt.version
-    else:
-        dict['ip_src'] = None
-        dict['ip_dst'] = None
-        dict['ip_ttl'] = None
-        dict['ip_len'] = None
-        dict['ip_ver'] = None
+ 
+    if pkt_data.haslayer(TCP):
+        print("TCP layer exists")
+        tcp_pkt = pkt_data.getlayer(TCP)
+        extract_tcp(parsed_dict, tcp_pkt)
 
-    dll_pkt = TCP(pkt_data)
-    if dll_pkt is not None:
-        # print("TCP dump")
-        # print(dll_pkt.show())
-        dict['dll_port'] = 'tcp'
-        dict['src_port'] = dll_pkt.sport
-        dict['dst_port'] = dll_pkt.dport
-    else:
-        dll_pkt = UDP(pkt_data)
-        if dll_pkt is not None:
-            # print("UPD dump")
-            # print(dll_pkt.show())
-            dict['dll_port'] = 'udp'
-            dict['src_port'] = dll_pkt.sport
-            dict['dst_port'] = dll_pkt.dport
-        else:
-            # print("Nothing here")
-            dict['dll_port'] = None
-            dict['src_port'] = None
-            dict['dst_port'] = None
+    if pkt_data.haslayer(UDP):
+        udp_pkt = pkt_data.getlayer(UDP)
+        extract_udp(parsed_dict, udp_pkt)
 
-    #TODO: Figure out why this doesn't work with live_capture
-    # Maybe because the captured live data there is already l2.Ether, while RawCapReader returns bytes
+        print("UDP layer exists")
+    if pkt_data.haslayer(ARP):
+        print("ARP layer exists")
+        arp_pkt = pkt_data.getlayer(ARP)
+        extract_arp(parsed_dict, arp_pkt)
+    if pkt_data.haslayer(ICMP):
+        icmp_pkt = pkt_data.getlayer(ICMP)
+        extract_icmp(parsed_dict, icmp_pkt)
 
-    # ether_pkt = Ether(pkt_data)
-    #
-    # if ether_pkt is not None:
-    #     dict['mac_src'] = ether_pkt.src
-    #     dict['mac_dst'] = ether_pkt.dst
-    #     print(ether_pkt.show())
-    # else:
-    #     dict['mac_src'] = None
-    #     dict['mac_dst'] = None
+        print("ICMP layer exists")
 
-    print("\nDictionary\n", dict)
+    print("Dictionary\n", parsed_dict)
+    print(pkt_data.summary())
     return
+
 
 
 count = 0
