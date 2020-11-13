131aff94838acd3e1f5e452ffd31fac73e911f40
Sahiti Bommareddy <sahiti@Sahitis-MBP.fios-router.home>
2020-11-12 21:01:28 -0500

    ml/packet/init changed

--- ml/packet/__init__.py
+++ ml/packet/__init__.py
@@ -4,54 +4,47 @@ They will be used by transform function to do feature engineering.
 """
 
 machines = {
-'128.220.221.91': ['00:22:4d:b8:6f:04', 4546],
-'128.220.221.92': ['00:22:4d:b8:6f:a5', 4546],
-'128.220.221.93': ['00:00:00:00:00:00', 4546],
-'128.220.221.94': ['00:22:4d:b8:70:0c', 4546],
-'128.220.221.95': ['00:22:4d:d0:88:58', 4546],
-'128.220.221.96': ['00:22:4d:d0:88:74', 4546],
-'128.220.221.15': ['00:22:4d:b5:86:75', 4546],
-'128.220.221.16': ['00:22:4d:b5:86:8b', 4546],
-'128.220.221.17': ['00:22:4d:b5:86:67', 4546],
-'128.220.221.1' : ['28:c0:da:7a:7f:81',0],
-'128.220.221.2' : ['d4:ae:52:c6:c2:34',0],
-'224.0.0.251'   : ['01:00:5e:00:00:fb',0],
-'255.255.255.255':['ff:ff:ff:ff:ff:ff',0],
-'128.220.221.5' : ['00:1c:73:0b:21:83',0],
-'169.254.10.173': ['00:22:4d:b5:87:f1',0],
-'0.0.0.0'       : ['58:97:1e:e0:d0:c0',0],
-'169.254.100.100': ['c4:04:15:b0:83:2c',0]
+    #Scada Masters
+    '128.220.221.91': ['00:22:4d:b8:6f:04', 4546],
+    '128.220.221.92': ['00:22:4d:b8:6f:a5', 4546],
+    '128.220.221.93': ['00:00:00:00:00:00', 4546],
+    '128.220.221.94': ['00:22:4d:b8:70:0c', 4546],
+    '128.220.221.95': ['00:22:4d:d0:88:58', 4546],
+    '128.220.221.96': ['00:22:4d:d0:88:74', 4546],
+    #Minis- plcs, hmis
+    '128.220.221.15': ['00:22:4d:b5:86:75', 4546],
+    '128.220.221.16': ['00:22:4d:b5:86:8b', 4546],
+    '128.220.221.17': ['00:22:4d:b5:86:67', 4546],
 }
 macs = [
-'00:22:4d:b8:6f:04',
-'00:22:4d:b8:6f:a5', 
-'00:00:00:00:00:00', 
-'00:22:4d:b8:70:0c', 
-'00:22:4d:d0:88:58',
-'00:22:4d:d0:88:74', 
-'00:22:4d:b5:86:8b', 
-'00:22:4d:b5:86:67', 
-'28:c0:da:7a:7f:81',
-'d4:ae:52:c6:c2:34',
-'01:00:5e:00:00:fb',
-'00:22:4d:b5:86:75',
-'ff:ff:ff:ff:ff:ff'
+    #Macs of above machines
+    '00:22:4d:b8:6f:04',
+    '00:22:4d:b8:6f:a5', 
+    '00:00:00:00:00:00', 
+    '00:22:4d:b8:70:0c', 
+    '00:22:4d:d0:88:58',
+    '00:22:4d:d0:88:74', 
+    '00:22:4d:b5:86:8b', 
+    '00:22:4d:b5:86:67', 
+    '28:c0:da:7a:7f:81',
 ]
 ports=[
-4546,
-53,
-34489,
-5353,
-68,123,
-34835
+    #Ports that are authorized to operate
+    4546,
+    53,
+    34489,
+    5353,
+    68,123,
+    34835
 ]
-
+#maps IPs to known and unknown set
 def do_ip(ip):
     val=1
     if ip in machines.keys():
         val=100
     return val
 
+#maps MACs to known and unknown set
 def do_mac(ip,mac):
     val=1
     if ip in machines.keys():
@@ -66,12 +59,15 @@ def do_only_mac(mac):
     if mac in macs:
         val=100
     return val
+
+#maps Ports to known and unknown set
 def do_ports(port):
     val=1
     if port in ports:
         val=100
     return val
 
+#Header features are transformed into vectors used for training
 def transform(row,fields):
     #print
     vec={}
@@ -134,5 +130,6 @@ def transform(row,fields):
         # print(vec)
     return vec
 
+#features selected after feature engineering. If we want to add or delete the field from feature vector we need to change this
 #distinct_cols=['ip_src', 'ip_dst', 'ip_ttl', 'ip_len', 'ip_ver', 'proto', 'mac_src', 'mac_dst', 'ether_type', 'tcp_src_port', 'tcp_dst_port', 'udp_src_port', 'udp_dst_port', 'udp_len', 'icmp_type', 'icmp_code', 'arp_op', 'arp_psrc', 'arp_pdst', 'arp_hwsrc', 'arp_hwdst', 'has_ip', 'has_ether', 'has_tcp', 'has_udp', 'has_icmp', 'has_arp']
 distinct_cols=['ip_src', 'ip_dst', 'ip_ttl', 'ip_len', 'ip_ver', 'proto', 'mac_src', 'mac_dst',  'tcp_src_port', 'tcp_dst_port', 'udp_src_port', 'udp_dst_port', 'icmp_type', 'icmp_code', 'arp_op', 'arp_psrc', 'arp_pdst', 'arp_hwsrc', 'arp_hwdst', 'has_ip', 'has_ether', 'has_tcp', 'has_udp', 'has_icmp', 'has_arp']
